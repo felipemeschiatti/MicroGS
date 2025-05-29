946e1321803700c2d80b92f697955048a49dbaa8
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-29 20:02:36 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/rm551978/globalsolution/GlobalsolutionApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.rm551978.globalsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalsolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobalsolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/rm551978/globalsolution/GlobalsolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.rm551978.globalsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobalsolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
